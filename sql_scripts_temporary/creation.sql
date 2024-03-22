a55c2630ee043a72d1fd5de0b52bb57eb3b4be26
MiroslavKlyukovsky <klyukovskymiroslav@gmail.com>
2024-03-22 13:11:44 +0200

    I added new quantity attribute quantity_at_start. Also added quantity to auto_part_archive. Updated individ. elem trigger logic.

--- sql_scripts_temporary/creation.sql
+++ sql_scripts_temporary/creation.sql
@@ -1,13 +1,10 @@
 /*
-product_code (always unique) - says brand and exact auto part purpose
-serial_code (not always unique) - says from which series is the auto part (factory, subtype of exact auto part)
-individual_auto_part_code (always unique) - identifies an individual auto part from all others
-*/
-/*
 -- problems can be not enough or too much constrains, difference between constraints, bad references
 -- Make triggers when one element is added to the archive, parent entities are added with a certain amount
 
 -- I might add quantity to auto_part archive, also I have to add trigger on adding consignments to add to auto_part quantity
+
+-- individual element moving in individual_auto_part_archive is not made yet, not automated
 */
 
 CREATE TABLE supplier (
@@ -61,7 +58,8 @@ CREATE TABLE auto_part (
 CREATE TABLE consignment (
     deal_code VARCHAR(12) PRIMARY KEY CHECK (deal_code ~ '^\d{12}$'),
     supplier_id INTEGER REFERENCES supplier(supplier_id) ON DELETE RESTRICT,
-    quantity SMALLINT NOT NULL CHECK (quantity > 0),
+    quantity_at_start SMALLINT NOT NULL CHECK (quantity_at_start > 0),
+    quantity SMALLINT NOT NULL CHECK (quantity >= 0),
     price_uah MONEY NOT NULL CHECK (price_uah < 3000000::MONEY),
     deal_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP CHECK (DATE(deal_time) = CURRENT_DATE),
     producing_factory VARCHAR(250) NOT NULL CHECK (producing_factory ~ '^[a-zA-Z0-9\s\-"''()\[\]]+$'),
@@ -80,13 +78,15 @@ CREATE TABLE individual_auto_part (
 CREATE TABLE auto_part_archive (
     product_code VARCHAR(100) PRIMARY KEY CHECK (LENGTH(product_code) >= 5),
     name VARCHAR(250) NOT NULL CHECK (LENGTH(name) >= 2),
+    quantity INTEGER NOT NULL CHECK (quantity >= 0), -- insert with brain usage
     warranty_term INTERVAL DAY NOT NULL CHECK (warranty_term >= INTERVAL '0 days' AND warranty_term <= INTERVAL '9000 days')
 );
 
 CREATE TABLE consignment_archive (
     deal_code VARCHAR(12) PRIMARY KEY CHECK (deal_code ~ '^\d{12}$'),
     supplier_id INTEGER REFERENCES supplier(supplier_id) ON DELETE RESTRICT,
-    quantity SMALLINT NOT NULL CHECK (quantity > 0),
+    quantity_at_start SMALLINT NOT NULL CHECK (quantity_at_start > 0),
+    quantity SMALLINT NOT NULL CHECK (quantity >= 0),
     price_uah MONEY NOT NULL CHECK (price_uah < 3000000::MONEY),
     deal_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP CHECK (DATE(deal_time) = CURRENT_DATE),
     producing_factory VARCHAR(250) NOT NULL CHECK (producing_factory ~ '^[a-zA-Z0-9\s\-"''()\[\]]+$'),
@@ -119,31 +119,94 @@ CREATE TABLE auto_part_compatible_car (
 );
 
 
+CREATE OR REPLACE FUNCTION add_quantity_on_new_consignment()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Increase the quantity of the corresponding auto_part
+    UPDATE auto_part
+    SET quantity = quantity + NEW.quantity
+    WHERE product_code = NEW.product_code;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create the trigger
+CREATE TRIGGER add_quantity_trigger
+AFTER INSERT ON consignment
+FOR EACH ROW
+EXECUTE FUNCTION add_quantity_on_new_consignment();
+
+/*
+ If we have auto parts and so their consignments and their rows in auto_part
+ This trigger finds product code for an individual auto part,
+ tries to create tables auto_part_archive and consignment_archive if they do not exist,
+ subtracts quantities by one from auto_part and consignment,
+ adds to quantities one in auto_part_archive and consignment_archive
+ deletes row from consignment if its quantity zero now
+ and then deletes auto_part row if its quantity zero (it has zero active consignments)
+*/
 CREATE OR REPLACE FUNCTION delete_individual_auto_part_trigger()
 RETURNS TRIGGER AS $$
 DECLARE
-    product_code_to_delete VARCHAR(100);
+    product_code_indiv VARCHAR(100);
 BEGIN
-    -- Retrieve the product code of the auto_part being deleted
-    SELECT a.product_code INTO product_code_to_delete
+    RAISE NOTICE 'Product Code Indiv: %', OLD.individual_auto_part_code;
+    /*
+    SELECT a.product_code INTO product_code_indiv
     FROM individual_auto_part i
     JOIN consignment c ON i.deal_code = c.deal_code
     JOIN auto_part a ON c.product_code = a.product_code
-    WHERE i.individual_auto_part_code = OLD.individual_auto_part_code;
+    WHERE i.individual_auto_part_code = OLD.individual_auto_part_code;*/
+    -- if I delete i.individual_auto_part_code no longer exist as I delete it and trigger was actovtaed
+    SELECT a.product_code INTO product_code_indiv
+    FROM consignment c
+    JOIN auto_part a ON c.product_code = a.product_code
+    WHERE c.deal_code = OLD.deal_code;
+    RAISE NOTICE 'Product Code Indiv: %', product_code_indiv;
 
-    -- Decrease the quantity of auto_part when an individual_auto_part is deleted
+    INSERT INTO auto_part_archive(product_code, name, quantity, warranty_term)
+    SELECT product_code, name, 0 AS quantity, warranty_term
+    FROM auto_part
+    WHERE auto_part.product_code = product_code_indiv
+      AND NOT EXISTS (
+      SELECT 1
+      FROM auto_part_archive
+      WHERE product_code_indiv = auto_part_archive.product_code
+    );
+
+    INSERT INTO consignment_archive(deal_code, supplier_id, quantity_at_start, quantity, price_uah, deal_time, producing_factory, product_code, serial_code)
+    SELECT deal_code, supplier_id, quantity_at_start, 0 AS quantity, price_uah, deal_time, producing_factory, product_code, serial_code
+    FROM consignment
+    WHERE deal_code = OLD.deal_code
+        AND NOT EXISTS (
+        SELECT 1
+        FROM consignment_archive
+        WHERE OLD.deal_code = consignment_archive.deal_code
+    );
+
+    UPDATE consignment
+    SET quantity = quantity - 1
+    WHERE deal_code = OLD.deal_code;
+
+    UPDATE consignment_archive
+    SET quantity = quantity + 1
+    WHERE deal_code = OLD.deal_code;
+
     UPDATE auto_part
     SET quantity = quantity - 1
-    WHERE product_code = product_code_to_delete;
+    WHERE product_code = product_code_indiv;
 
-    -- Check if the quantity reaches zero and move the auto_part to archive if needed
-    IF (SELECT quantity FROM auto_part WHERE product_code = product_code_to_delete) <= 0 THEN
-        INSERT INTO auto_part_archive(product_code, name, warranty_term)
-        SELECT product_code, name, warranty_term
-        FROM auto_part
-        WHERE product_code = product_code_to_delete;
+    UPDATE auto_part_archive
+    SET quantity = quantity + 1
+    WHERE product_code = product_code_indiv;
 
-        DELETE FROM auto_part WHERE product_code = product_code_to_delete;
+    IF (SELECT quantity FROM consignment WHERE deal_code = OLD.deal_code) = 0 THEN
+        DELETE FROM consignment WHERE deal_code = OLD.deal_code;
+    END IF;
+
+    IF (SELECT quantity FROM auto_part WHERE auto_part.product_code = product_code_indiv) = 0 THEN
+        DELETE FROM auto_part WHERE auto_part.product_code = product_code_indiv;
     END IF;
 
     RETURN OLD;
@@ -154,3 +217,6 @@ CREATE TRIGGER delete_individual_auto_part
 AFTER DELETE ON individual_auto_part
 FOR EACH ROW
 EXECUTE FUNCTION delete_individual_auto_part_trigger();
+
+
+
